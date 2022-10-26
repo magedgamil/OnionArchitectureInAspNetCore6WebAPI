59c0ecc5654e98f59d0130a5017ceb8cae283059
maged gamil <magedgamil8@gmail.com>
2022-10-26 22:47:27 +0200

    first commit

---
+++ RepositoryLayer/Repository/Repository.cs
@@ -0,0 +1,73 @@
+﻿using DomainLayer.Data;
+using DomainLayer.Models;
+using Microsoft.EntityFrameworkCore;
+using RepositoryLayer.IRepository;
+namespace RepositoryLayer.Repository
+{
+    public class Repository<T> : IRepository<T> where T : BaseEntity
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+        private DbSet<T> entities;
+
+        public Repository(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+            entities = _applicationDbContext.Set<T>();
+        }
+
+        public void Delete(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+            entities.Remove(entity);
+            _applicationDbContext.SaveChanges();
+        }
+
+        public T Get(int Id)
+        {
+            return entities.SingleOrDefault(c => c.Id == Id);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return entities.AsEnumerable();
+        }
+
+        public void Insert(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+             entities.Add(entity);
+            _applicationDbContext.SaveChanges();
+        }
+
+        public void Remove(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+            entities.Remove(entity);
+        }
+
+        public void SaveChanges()
+        {
+            _applicationDbContext.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+            entities.Update(entity);
+            _applicationDbContext.SaveChanges();
+        }
+
+    }
+}
